08d10b602162e6d35db892b13c133b77447f953f
Shiazaki <144890059+Shiazaki@users.noreply.github.com>
2024-01-10 10:24:25 +0100

    txt fájl adataival
    ez még semmi csak adatok. Valóstínüleg nem is jó így

---
+++ ipariforr.py
@@ -0,0 +1,6 @@
+ adat = {
+     'Edison' : 1878; 'fonográf'; 'Thomas Edison'; 'Amerikai Egyesült Államok',
+     'Fulton' : 1807; 'gőzhajó'; 'Robert Fulton'; 'Amerikai Egyesült Államok';
+     }
+ 
+ 
