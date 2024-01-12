37bfaff66de82cad046c37b7e45e718504617183
Shiazaki <144890059+Shiazaki@users.noreply.github.com>
2024-01-12 13:30:42 +0100

    updated code (Shizzy's)

--- ipariforr.py
+++ ipariforr.py
@@ -1,6 +1,14 @@
- adat = {
-     'Edison' : 1878; 'fonográf'; 'Thomas Edison'; 'Amerikai Egyesült Államok',
-     'Fulton' : 1807; 'gőzhajó'; 'Robert Fulton'; 'Amerikai Egyesült Államok';
-     }
+adat = []
+sor = []
+with open('ikt.txt', 'r', encoding='utf-8') as fajl1:
+    for sor in fajl1:
+        adat.append(sor.strip())
  
+print(adat)
  
+menu = int(input("1. Minden adat listázása a képernyőre\n2. Minden adat listázása egy fájlba \n3. Adott időtartományban lévő vívmányok összes adatának listázása\n4. Tudásteszt\n \t"))
+
+if menu == 1 or 1.:
+    print(adat.items())
+    elif menu == 2:
+        
